0d12bd4399b80b0df78935e8f3f5069aa4a9ce43
jirehinformatica <sistemas@vieiramachado.adv.br>
2015-08-06 18:02:59 -0300

    Extrato 5

--- JirehComprovante/Extrato.Designer.vb
+++ JirehComprovante/Extrato.Designer.vb
@@ -22,16 +22,138 @@ Partial Class Extrato
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.tsMenu = New System.Windows.Forms.ToolStrip()
+        Me.tsbAbrir = New System.Windows.Forms.ToolStripButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.opArquivo = New System.Windows.Forms.OpenFileDialog()
+        Me.tsMenu.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'tsMenu
+        '
+        Me.tsMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbAbrir})
+        Me.tsMenu.Location = New System.Drawing.Point(0, 0)
+        Me.tsMenu.Name = "tsMenu"
+        Me.tsMenu.Size = New System.Drawing.Size(730, 38)
+        Me.tsMenu.TabIndex = 0
+        Me.tsMenu.Text = "ToolStrip1"
+        '
+        'tsbAbrir
+        '
+        Me.tsbAbrir.Image = Global.JirehComprovante.My.Resources.Resources.Open24x24
+        Me.tsbAbrir.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.tsbAbrir.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbAbrir.Name = "tsbAbrir"
+        Me.tsbAbrir.Size = New System.Drawing.Size(37, 35)
+        Me.tsbAbrir.Text = "Abrir"
+        Me.tsbAbrir.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.tsbAbrir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.tsbAbrir.ToolTipText = "Abrir arquivo extrato"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ComboBox3)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.ComboBox2)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 38)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(730, 117)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(265, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Conta Corrente"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(268, 26)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(250, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(12, 26)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(250, 21)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Empresa"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(524, 26)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(194, 21)
+        Me.ComboBox3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(521, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Nº Sequência"
         '
         'Extrato
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1021, 494)
+        Me.ClientSize = New System.Drawing.Size(730, 494)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.tsMenu)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.KeyPreview = True
         Me.Name = "Extrato"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Extrato"
+        Me.tsMenu.ResumeLayout(False)
+        Me.tsMenu.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents tsMenu As System.Windows.Forms.ToolStrip
+    Friend WithEvents tsbAbrir As System.Windows.Forms.ToolStripButton
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents opArquivo As System.Windows.Forms.OpenFileDialog
 End Class
--- JirehComprovante/Extrato.vb
+++ JirehComprovante/Extrato.vb
@@ -1,3 +1,56 @@
 ﻿Public Class Extrato
 
+    Protected WithEvents Eventos As JirehLayouts.JirehLayoutEventos
+    Private Empresa As Dictionary(Of String, String)
+    Private Conta As Dictionary(Of String, String)
+
+
+    Private Sub tsbAbrir_Click(sender As Object, e As EventArgs) Handles tsbAbrir.Click
+        Try
+            opArquivo.Title = "Abrir o arquivo de extrato do banco."
+            opArquivo.Filter = "Arquivo extrato|*.ret;Arquivo extrato|*.txt"
+            opArquivo.Multiselect = False
+
+            Dim resp As DialogResult = opArquivo.ShowDialog
+
+            If resp = Windows.Forms.DialogResult.OK OrElse resp = Windows.Forms.DialogResult.Yes Then
+
+                FormularioMDI.SetProgresso(True)
+
+                Eventos = New JirehLayouts.JirehLayoutEventos
+                Dim LerArquivo As New JirehLayouts.ExtratoCNAB240(opArquivo.FileName, Eventos)
+
+                If Not LerArquivo.ArquivoValido(JirehBDUtil.InfoRegistro.InformacoesServidor.GetConveniosString) AndAlso CanceladoPorUsuario Then
+                    Exit Sub
+                End If
+
+                Empresa = New Dictionary(Of String, String)
+                Conta = New Dictionary(Of String, String)
+
+
+            End If
+
+        Catch ex As Exception
+            TratarErros(ex)
+        End Try
+    End Sub
+
+    Private Sub Eventos_Mensagem(sender As Object, e As JirehLayouts.LayoutPendenciaEventArgs) Handles Eventos.Mensagem
+        Try
+            Alerta(e.Mensagem)
+        Catch ex As Exception
+            TratarErros(ex)
+        End Try
+    End Sub
+
+    Private Sub Eventos_Progresso(sender As Object, ByRef e As JirehLayouts.LayoutEventosEventArgs) Handles Eventos.Progresso
+        Try
+            FormularioMDI.SetProgresso(e.Progresso, e.MaximoValor)
+            FormularioMDI.SetPrompt(e.Mensagem, e.Progresso)
+            e.Cancelar = CanceladoPorUsuario
+        Catch ex As Exception
+            TratarErros(ex)
+        End Try
+    End Sub
+
 End Class
